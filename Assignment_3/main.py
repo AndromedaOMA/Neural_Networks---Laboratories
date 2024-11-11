071b8b14f766fee8cb7f965cefcf61b354c2728c
Olaru Marius-Alexandru <marius.alexandru.olaru@gmail.com>
2024-11-11 14:52:54 +0200

    added the asgn3

--- Assignment_3/main.py
+++ Assignment_3/main.py
@@ -2,6 +2,8 @@ import numpy as np
 from torchvision.datasets import MNIST
 import time
 import matplotlib.pyplot
+
+
 # %matplotlib inline
 
 
@@ -16,18 +18,17 @@ def download_mnist(is_train: bool):
         mnist_data.append(image)
         mnist_labels.append(label)
 
-    mnist_data = np.array(mnist_data)
+    mnist_data = (np.array(mnist_data) / 255.0 * 0.99) + 0.01
     mnist_labels = np.array(mnist_labels)
 
-    encoded_labels = np.zeros((len(mnist_labels), max(mnist_labels)+1), dtype=int)
+    encoded_labels = np.zeros((len(mnist_labels), max(mnist_labels) + 1), dtype=int)
     encoded_labels[np.arange(len(mnist_labels)), mnist_labels] = 1
     return mnist_data, encoded_labels
 
 
-train_x, train_y = download_mnist(True) #60000
+train_x, train_y = download_mnist(True)  #60000
 test_x, test_y = download_mnist(False)  #10000
-
-
+print(f"Here we got the #row  {len(train_x)} and #col: {len(train_x[0])} of training data.\n")
 print(f"Here we got the length of the 99th training data: {len(train_x[99])},\n"
       f" The 99th training data: {train_x[99]},\n The 99th training label: {train_y[99]},\n")
 
@@ -40,6 +41,23 @@ def batches_generator(train_data, train_labels, no_of_batches):
     for i in range(0, len(train_data), no_of_batches):
         batch_indices = indices[i:i + no_of_batches]
         yield train_data[batch_indices], train_labels[batch_indices]
+
+
+def sigmoid(x, backpropagation=False):
+    # Clip values to prevent overflow
+    x = np.clip(x, -500, 500)  # Clip x to avoid large values
+    s = 1 / (1 + np.exp(-x))
+    if backpropagation:
+        return s * (1 - s)
+    return s
+
+
+def softmax(x, backpropagation=False):
+    exp = np.exp(x - np.max(x))
+    s = exp / np.sum(exp, axis=0)
+    if backpropagation:
+        return s * (1 - s)
+    return s
 
 
 class NN:
@@ -58,19 +76,53 @@ class NN:
         """configurare Xavier pentru model ce utilizează funcții simetrice
            (tocmai pentru evitarea blocării rețelei neuronale)"""
         self.params = {
-            'W1': np.random.randn(h_layer, in_layer) * np.sqrt(2 / (h_layer + in_layer)),
-            'W2': np.random.randn(out_layer, h_layer) * np.sqrt(2 / (out_layer + h_layer))
+            'W1': np.random.randn(h_layer, in_layer) * np.sqrt(2 / (h_layer + in_layer)),  #100x784
+            'W2': np.random.randn(out_layer, h_layer) * np.sqrt(2 / (out_layer + h_layer))  #10x100
         }
 
-    def forward_prop(self):
-        pass
+    def forward_prop(self, x_train):
+        params = self.params
 
-    def backward_prop(self):
-        pass
+        params['A0'] = x_train  #784x1
+        params['Z1'] = np.dot(params['W1'], params['A0'])  #100x1
+        params['A1'] = sigmoid(params['Z1'])  #100x1
+        params['Z2'] = np.dot(params['W2'], params['A1'])  #10x1
+        params['A2'] = softmax(params['Z2'])  #10x1
 
-    def compute_acc(self):
-        pass
+        return params['A2']
 
-    def update_w(self):
-        pass
+    def backward_prop(self, y_train, output):
+        params = self.params
+
+        # err = output - y_train
+        err = (output - y_train) * softmax(params['Z2'], backpropagation=True)
+        params['W2'] -= self.learning_rate * np.outer(err, params['A1'])
+
+        err = np.dot(params['W2'].T, err) * sigmoid(params['Z1'], backpropagation=True)
+        params['W1'] -= self.learning_rate * np.outer(err, params['A0'])
+
+    def compute_acc(self, test_data, test_labels):
+        predictions = []
+        # for data_batch, label_batch in batches_generator(test_data, test_labels, self.batches):
+        for i in range(len(test_data)):
+            output = self.forward_prop(test_data[i])
+            predict = np.argmax(output)
+            predictions.append(predict == np.argmax(test_labels[i]))
+        return np.mean(predictions)
+
+    def train(self, train_list, train_labels, test_list, test_labels):
+        start_time = time.time()
+        for i in range(self.epochs):
+            # for data_batch, label_batch in batches_generator(train_list, train_labels, self.batches):
+            for j in range(len(train_list)):
+                output = self.forward_prop(train_list[j])
+                self.backward_prop(train_labels[j], output)
+
+            accuracy = self.compute_acc(test_list, test_labels)
+            print(f'Epoch: {i + 1}, Time Spent: {time.time() - start_time}s, Accuracy: {accuracy * 100}%')
+
+
+if __name__ == "__main__":
+    nn = NN()
+    nn.train(train_x, train_y, test_x, test_y)
 
